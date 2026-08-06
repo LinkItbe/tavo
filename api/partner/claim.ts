b87067fe133b245edacac5b29021695fb0ca64d3
LinkItbe <ruslan.suhaka.a@gmail.com>
2026-08-06 03:03:34 +0300

    Update claim.ts

--- api/partner/claim.ts
+++ api/partner/claim.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { claimPartnerAccount } from '../../src/server/partner';
+import { claimPartnerAccount } from '../../src/server/partner.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Content-Type', 'application/json');
